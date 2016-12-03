990c787379ddb229547c2324d87376bd1755a2ef
pacocampo <paco.ocampor@gmail.com>
2016-12-03 14:00:48 -0600

    Graph API added

--- facebook-demo/ViewController.swift
+++ facebook-demo/ViewController.swift
@@ -10,14 +10,32 @@ import UIKit
 import FBSDKLoginKit
 
 class ViewController: UIViewController {
+  
 
   override func viewDidLoad() {
     super.viewDidLoad()
     
+    // Facebook Button
     let loginButton : FBSDKLoginButton = FBSDKLoginButton()
+    loginButton.readPermissions = ["email", "public_profile"]
     loginButton.center = self.view.center
     self.view.addSubview(loginButton)
     
+    // Access Token
+    if let token = FBSDKAccessToken.current() {
+      let req = FBSDKGraphRequest(graphPath: "me", parameters: ["fields":"email,name"], tokenString: token.tokenString, version: nil, httpMethod: "GET")
+      req!.start(completionHandler: { (connection, result, error) in
+        if(error == nil) {
+            print("result \(result!)")
+          } else {
+            print("error \(error)")
+          }
+      })
+    }
+  }
+  
+  override func viewDidAppear(_ animated: Bool) {
+
   }
 
   override func didReceiveMemoryWarning() {
